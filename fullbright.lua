77d92bf6bf5e2801042c332a0c65d486549eab87
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-03-28 22:36:34 +0800

    Update fullbright.lua

--- fullbright.lua
+++ fullbright.lua
@@ -1,8 +1,8 @@
 local function enableFullbright()
     game:GetService("Lighting").GlobalShadows = false
-    game:GetService("Lighting").Brightness = 5 -- Adjust the brightness value as needed
-    game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128) -- Adjust ambient lighting color as needed
-    game:GetService("Lighting").FogEnd = 100000 -- Increase fog distance to ensure visibility
+    game:GetService("Lighting").Brightness = 2
+    game:GetService("Lighting").OutdoorAmbient = Color3.fromRGB(128, 128, 128)
+    game:GetService("Lighting").FogEnd = 100000
 end
 
 enableFullbright()
